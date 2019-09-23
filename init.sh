bb876ed0d2b36599f6cd197d63daf1ab2560c894
Artem Uvakin <artemuvakin@gmail.com>
2019-09-23 11:48:38 +0300

    2019 правки

--- init.sh
+++ init.sh
@@ -1,6 +1,2 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test.conf
-sudo /etc/init.d/gunicorn stop
-sudo gunicorn -c etc/gunicorn.conf --error-logfile /var/log/gunicorn/error.log --pythonpath /home/box/web hello:app -D
-sudo /etc/init.d/mysql start
