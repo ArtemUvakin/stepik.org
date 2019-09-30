160da8c883d8f4ec5ad8df06f4f3b39ec33ac6a1
Artem Uvakin <artemuvakin@gmail.com>
2019-09-30 20:13:33 +0300

    3 урок начало

--- init.sh
+++ init.sh
@@ -1,2 +1,8 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+unlink /etc/nginx/sites-enabled/default
+ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
+cd /home/box/web/
+django-admin startproject ask
+cd ask/
+django-admin startproject qa
+gunicorn -c /home/box/web/etc/hello.py hello:app
